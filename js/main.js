efa24f8e1b2cd09b265ce8e55ca34dda27b05e42
somukhan9 <67901700+somukhan9@users.noreply.github.com>
2020-10-31 15:59:01 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// BX Slider
+$(document).ready(function(){
+  $('.slider').bxSlider();
+    jQuery('.main_nav').meanmenu({
+        meanMenuContainer: '.mobile_menu',
+        meanScreenWidth: 960
+    })
+});
